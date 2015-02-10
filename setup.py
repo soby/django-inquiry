22bb4900e365a8d3a15eed58d4896b918b146b5a
soby <sobyx@hotmail.com>
2015-02-09 18:43:46 -0800

    packaging stuff

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-inquiry',
+    version='0.1',
+    packages=['inquiry'],
+    include_package_data=True,
+    license='BSD License',
+    description='Django app for surveys, lists, and training',
+    long_description=README,
+    url='',
+    author='Brian Soby',
+    author_email='sobyx@hotmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
